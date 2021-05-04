ac4b6bb1a606898f9092ff02f94ee247983e32f5
tinkerjoe-git <nicholas.stagno@gmail.com>
2021-05-04 12:57:29 -0400

    commit tree 3563c0ed33c5761236872f01a94131bb7fd513ad

--- index.js
+++ index.js
@@ -11,3 +11,25 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+  let time_array = time.split(':');
+  let h = parseInt(time_array[0])
+  let m = parseInt(time_array[1])
+  if (h < 12) {
+    return 'Good Morning';
+  }
+  else if (h < 17 && h > 12) {
+    return 'Good Afternoon';
+  }
+  else if (h > 17) {
+    return 'Good Evening';
+  }
+  return "Good Afternoon"
+}
+
+
+
+function displayMessage(msg){
+  document.getElementById("greeting").innerText = msg;
+}
